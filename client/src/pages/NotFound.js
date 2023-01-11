23e91bf189b9288a43220aab33fbd602569a35b3
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-11 18:32:18 +0300

    add 404 page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Button, Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>NotFound</div>
-  )
+    <div className="h-screen flex justify-center items-center bg-slate-200">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={() => navigate('/')}>
+            Back Home
+          </Button>
+        }
+      />
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
