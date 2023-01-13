345f815975e9099a1929c627be16b934888c1cda
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-13 15:49:02 +0300

    update url

--- server/app.js
+++ server/app.js
@@ -16,7 +16,11 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://etecube-assignment-server.onrender.com'],
+  })
+);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
